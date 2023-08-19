1af167157e28c42af13a64c3cbdfa253fa70cee4
hisakazu <cantabilehisa@gmail.com>
2023-08-20 01:01:46 +0900

    kover, exclude generated files

--- build-logic/src/main/kotlin/io/github/droidkaigi/confsched2023/primitive/KoverPlugin.kt
+++ build-logic/src/main/kotlin/io/github/droidkaigi/confsched2023/primitive/KoverPlugin.kt
@@ -1,12 +1,32 @@
 package io.github.droidkaigi.confsched2023.primitive
 
+import kotlinx.kover.gradle.plugin.dsl.KoverProjectExtension
+import kotlinx.kover.gradle.plugin.dsl.KoverReportExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
 
+@Suppress("unused")
 class KoverPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with (target) {
             pluginManager.apply("org.jetbrains.kotlinx.kover")
+            extensions.configure<KoverProjectExtension>("kover") { }
+            extensions.configure<KoverReportExtension>("koverReport") {
+                filters {
+                    excludes {
+                        packages(
+                            "com.airbnb.android.showkase",
+                            "hilt_aggregated_deps",
+                        )
+                    }
+                }
+                defaults {
+                    verify {
+                        rule { }
+                    }
+                }
+            }
         }
     }
 }
